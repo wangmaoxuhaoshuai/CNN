0b3a1b7ee038b5fd6507b48e52379bb5d09de8c9
wangmaoxuPCX1 <guangyuanwangmaoxu@163.com>
2019-05-08 22:09:00 +0800

    add function for MNIST test in Predict.py and update codes for CNN

--- FirstCNN/CNN.py
+++ FirstCNN/CNN.py
@@ -3,78 +3,80 @@ import os
 from tensorflow.examples.tutorials.mnist import input_data
 
 #
-mnist = input_data.read_data_sets('../MNIST_data/', one_hot=True)
-train_img = mnist.train.images
-train_label = mnist.train.labels
-test_img = mnist.test.images
-test_label = mnist.test.images
+def NetworkFramework():
+    x = tf.placeholder(tf.float32, [None, 784])
+    y = tf.placeholder(tf.float32, [None, 10])
 
-print("MNIST loaded")
+    def weight_var(shape, name):
+        return tf.get_variable(name, shape, initializer=tf.contrib.layers.xavier_initializer())
 
-#
-x = tf.placeholder(tf.float32, [None, train_img.shape[1]])
-y = tf.placeholder(tf.float32, [None, train_label.shape[1]])
+    def bias_var(shape, name):
+        return tf.get_variable(name, shape, initializer=tf.constant_initializer(0))
 
-def weight_var(shape, name):
-    return tf.get_variable(name, shape, initializer=tf.contrib.layers.xavier_initializer())
+    def conv2d(x, W):
+        return tf.nn.conv2d(x, W, strides=[1, 1, 1, 1], padding='SAME')
 
-def bias_var(shape, name):
-    return tf.get_variable(name, shape, initializer=tf.constant_initializer(0))
+    def max_pol_2x2(x):
+        return tf.nn.max_pool(x, ksize=[1, 2, 2, 1], strides=[1, 2, 2, 1], padding='SAME')
 
-def conv2d(x, W):
-    return tf.nn.conv2d(x, W, strides=[1, 1, 1, 1], padding='SAME')
+    x_image = tf.reshape(x, [-1, 28, 28, 1])
 
-def max_pol_2x2(x):
-    return tf.nn.max_pool(x, ksize=[1, 2, 2, 1], strides=[1, 2, 2, 1], padding='SAME')
+    W_conv1 = weight_var([5, 5, 1, 6], 'W_conv1')
+    b_conv1 = bias_var([6], 'b_conv1')
+    h_conv1 = tf.nn.relu(conv2d(x_image, W_conv1) + b_conv1)
+    h_pool1 = max_pol_2x2(h_conv1)
 
-x_image = tf.reshape(x, [-1, 28, 28, 1])
+    W_conv2 = weight_var([5, 5, 6, 16], 'W_conv2')
+    b_conv2 = bias_var([16], 'b_con2')
+    h_conv2 = tf.nn.relu(conv2d(h_pool1, W_conv2) + b_conv2)
+    h_pool2 = max_pol_2x2(h_conv2)
 
-W_conv1 = weight_var([5, 5, 1, 6], 'W_conv1')
-b_conv1 = bias_var([6], 'b_conv1')
-h_conv1 = tf.nn.relu(conv2d(x_image, W_conv1) + b_conv1)
-h_pool1 = max_pol_2x2(h_conv1)
+    W_conv3 = weight_var([5, 5, 16, 120], 'W_conv3')
+    b_conv3 = bias_var([120], 'b_conv3')
+    h_conv3 = tf.nn.relu(conv2d(h_pool2, W_conv3) + b_conv3)
+    h_pool3_flat = tf.reshape(h_conv3, [-1, 7 * 7 * 120])
 
-W_conv2 = weight_var([5, 5, 6, 16], 'W_conv2')
-b_conv2 = bias_var([16], 'b_con2')
-h_conv2 = tf.nn.relu(conv2d(h_pool1, W_conv2) + b_conv2)
-h_pool2 = max_pol_2x2(h_conv2)
+    W_fcl1 = weight_var([7 * 7 * 120, 1024], 'W_fcl1')
+    b_fcl1 = bias_var([1024], 'b_fcl1')
+    h_fcl1 = tf.nn.relu(tf.nn.xw_plus_b(h_pool3_flat, W_fcl1, b_fcl1))
+    keep_prob = tf.placeholder(tf.float32)
+    h_fcl1_drop = tf.nn.dropout(h_fcl1, keep_prob)
 
-W_conv3 = weight_var([5, 5, 16, 120], 'W_conv3')
-b_conv3 = bias_var([120], 'b_conv3')
-h_conv3 = tf.nn.relu(conv2d(h_pool2, W_conv3) + b_conv3)
-h_pool3_flat = tf.reshape(h_conv3, [-1, 7*7*120])
+    W_fcl2 = weight_var([1024, 10], 'W_fcl2')
+    b_fcl2 = bias_var([10], 'b_fcl2')
+    y_conv = tf.nn.softmax(tf.nn.xw_plus_b(h_fcl1_drop, W_fcl2, b_fcl2))
 
-W_fcl1 = weight_var([7*7*120, 1024], 'W_fcl1')
-b_fcl1 = bias_var([1024], 'b_fcl1')
-h_fcl1 = tf.nn.relu(tf.nn.xw_plus_b(h_pool3_flat, W_fcl1, b_fcl1))
-keep_prob = tf.placeholder(tf.float32)
-h_fcl1_drop = tf.nn.dropout(h_fcl1, keep_prob)
+    var_list = [
+        W_conv1, b_conv1,
+        W_conv2, b_conv2,
+        W_conv3, b_conv3,
+        W_fcl1, b_fcl1,
+        W_fcl2, b_fcl2
+    ]
 
-W_fcl2 = weight_var([1024, 10], 'W_fcl2')
-b_fcl2 = bias_var([10], 'b_fcl2')
-y_conv = tf.nn.softmax(tf.nn.xw_plus_b(h_fcl1_drop, W_fcl2, b_fcl2))
+    return x, y, y_conv, var_list, keep_prob
 
-var_list = [
-    W_conv1, b_conv1,
-    W_conv2, b_conv2,
-    W_conv3, b_conv3,
-    W_fcl1, b_fcl1,
-    W_fcl2, b_fcl2
-]
+def main():
+    #
+    mnist = input_data.read_data_sets('../MNIST_data/', one_hot=True)
 
-loss = tf.reduce_mean(tf.nn.sigmoid_cross_entropy_with_logits(logits=y_conv, labels=y))
-optimizer = tf.train.AdamOptimizer(1e-4).minimize(loss)
+    print("MNIST loaded")
 
-correct_prediction = tf.equal(tf.argmax(y_conv, 1), tf.argmax(y, 1))
-accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
+    x, y, y_conv, var_list, keep_prob = NetworkFramework()
 
-if not os.path.exists('Model/'):
-    os.mkdir('Model/')
+    loss = tf.reduce_mean(tf.nn.sigmoid_cross_entropy_with_logits(logits=y_conv, labels=y))
+    optimizer = tf.train.AdamOptimizer(1e-4).minimize(loss)
 
-saver = tf.train.Saver(var_list)
-init = tf.global_variables_initializer()
+    correct_prediction = tf.equal(tf.argmax(y_conv, 1), tf.argmax(y, 1))
+    accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
 
-with tf.Session() as sess:
+    if not os.path.exists('Model/'):
+        os.mkdir('Model/')
+
+    saver = tf.train.Saver(var_list)
+    init = tf.global_variables_initializer()
+
+    sess = tf.InteractiveSession()
     sess.run(init)
     for i in range(100000):
         batch = mnist.train.next_batch(60)
@@ -92,3 +94,6 @@ with tf.Session() as sess:
         })
     saver.save(sess, 'Model/model.ckpt')
     print('test accuracy %g' % accuracy.eval(feed_dict={x: mnist.test.images, y: mnist.test.labels, keep_prob: 1.0}))
+
+if __name__ == '__main__':
+    main()
--- FirstCNN/Predict.py
+++ FirstCNN/Predict.py
@@ -1,70 +1,24 @@
 import tensorflow as tf
 from PIL import Image, ImageFilter
+from CNN import NetworkFramework
 import os
-tf.reset_default_graph()
+from tensorflow.examples.tutorials.mnist import input_data
+
 os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'
+
 def predictInt(imgValue):
-
-    x = tf.placeholder(tf.float32, [None, 784])
-
-    def weight_var(shape, name):
-        return tf.get_variable(name, shape, initializer=tf.contrib.layers.xavier_initializer())
-
-    def bias_var(shape, name):
-        return tf.get_variable(name, shape, initializer=tf.constant_initializer(0))
-
-    def conv2d(x, W):
-        return tf.nn.conv2d(x, W, strides=[1, 1, 1, 1], padding='SAME')
-
-    def max_pol_2x2(x):
-        return tf.nn.max_pool(x, ksize=[1, 2, 2, 1], strides=[1, 2, 2, 1], padding='SAME')
-
-    x_image = tf.reshape(x, [-1, 28, 28, 1])
-
-    W_conv1 = weight_var([5, 5, 1, 6], 'W_conv1')
-    b_conv1 = bias_var([6], 'b_conv1')
-    h_conv1 = tf.nn.relu(conv2d(x_image, W_conv1) + b_conv1)
-    h_pool1 = max_pol_2x2(h_conv1)
-
-    W_conv2 = weight_var([5, 5, 6, 16], 'W_conv2')
-    b_conv2 = bias_var([16], 'b_con2')
-    h_conv2 = tf.nn.relu(conv2d(h_pool1, W_conv2) + b_conv2)
-    h_pool2 = max_pol_2x2(h_conv2)
-
-    W_conv3 = weight_var([5, 5, 16, 120], 'W_conv3')
-    b_conv3 = bias_var([120], 'b_conv3')
-    h_conv3 = tf.nn.relu(conv2d(h_pool2, W_conv3) + b_conv3)
-    h_pool3_flat = tf.reshape(h_conv3, [-1, 7 * 7 * 120])
-
-    W_fcl1 = weight_var([7 * 7 * 120, 1024], 'W_fcl1')
-    b_fcl1 = bias_var([1024], 'b_fcl1')
-    h_fcl1 = tf.nn.relu(tf.nn.xw_plus_b(h_pool3_flat, W_fcl1, b_fcl1))
-    keep_prob = tf.placeholder(tf.float32)
-    h_fcl1_drop = tf.nn.dropout(h_fcl1, keep_prob)
-
-    W_fcl2 = weight_var([1024, 10], 'W_fcl2')
-    b_fcl2 = bias_var([10], 'b_fcl2')
-    y_conv = tf.nn.softmax(tf.nn.xw_plus_b(h_fcl1_drop, W_fcl2, b_fcl2))
-
-    var_list = [
-        W_conv1, b_conv1,
-        W_conv2, b_conv2,
-        W_conv3, b_conv3,
-        W_fcl1, b_fcl1,
-        W_fcl2, b_fcl2
-    ]
+    tf.reset_default_graph()
+    x, y, y_conv, var_list, keep_prob = NetworkFramework()
 
     init = tf.global_variables_initializer()
     saver = tf.train.Saver(var_list)
 
-    with tf.Session() as sess:
-        sess.run(init)
-        saver.restore(sess, 'Model/model.ckpt')
+    sess = tf.InteractiveSession()
+    sess.run(init)
+    saver.restore(sess, 'Model/model.ckpt')
 
-        prediction = tf.argmax(y_conv, 1)
-        result = prediction.eval(feed_dict={x: [imgValue], keep_prob: 1.0}, session=sess)
-    sess.close()
-    return result
+    prediction = tf.argmax(y_conv, 1)
+    return prediction.eval(feed_dict={x: [imgValue], keep_prob: 0.5}, session=sess)
 
 def imageprepare(argv):
     #Image.convert() 转换图像 'L'表示灰度图像
@@ -96,11 +50,35 @@ def imageprepare(argv):
     tva = [(255 - x) * 1.0 / 255.0 for x in tv]
     return tva
 
-def main(argv):
-    imvalue = imageprepare(argv)
-    predint = predictInt(imvalue)
-    print(predint[0])
+# use our own image for test
+def main():
+    for i in range(10):
+        imvalue = imageprepare('Test_data/%d.jpg'%(int(i)))
+        predint = predictInt(imvalue)
+        print(predint[0])
+
+# use MNIST for test
+def MNIST_test():
+    mnist = input_data.read_data_sets('../MNIST_data/', one_hot=True)
+
+    x, y, y_conv, var_list, keep_prob = NetworkFramework()
+
+    correct_prediction = tf.equal(tf.argmax(y_conv, 1), tf.argmax(y, 1))
+    accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
+
+    init = tf.global_variables_initializer()
+    saver = tf.train.Saver(var_list)
+
+    sess = tf.InteractiveSession()
+    sess.run(init)
+    saver.restore(sess, 'Model/model.ckpt')
+
+    for i in range(100000):
+        batch = mnist.test.next_batch(60)
+        if i % 1000 == 0:
+            print('test accuracy %g' % sess.run(accuracy, feed_dict={x: batch[0], y: batch[1], keep_prob: 0.5}))
 
 if __name__ == '__main__':
-    main('Test_data/9.jpg')
+    # main()
+    MNIST_test()
 
