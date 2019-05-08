c65d895a335e82630726b86edd7135d2e1b5cc13
wangmaoxuPCX1 <guangyuanwangmaoxu@163.com>
2019-05-08 17:16:44 +0800

    delete Test.py

--- FirstCNN/Test.py
+++
@@ -1,7 +0,0 @@
-from Predict import imageprepare, predictInt
-
-if __name__ == '__main__':
-    for i in range(10):
-        imvalue = imageprepare('Test_data/%d.jpg' % (int(i)))
-        predint = predictInt(imvalue)
-        print(predint[0])
